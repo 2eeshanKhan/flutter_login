1ebd03bff293b2167b4d15f25e94af1a8e88f64c
2eeshanKhan <thejisankhan20@gmail.com>
2023-03-04 10:53:27 +0530

    first commit

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../utils/color_utils.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("CB2B93"),
+          hexStringToColor("9546C4"),
+          hexStringToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/logo1.png"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Image logoWidget(String imageName) {
+    return Image.asset(
+      imageName,
+      fit: BoxFit.fitWidth,
+      width: 240,
+      height: 240,
+      color: Colors.white,
+    );
+  }
+}
