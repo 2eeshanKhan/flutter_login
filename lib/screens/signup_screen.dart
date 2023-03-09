7260afba183b842bd9c93883a774293226a0c6ca
2eeshanKhan <thejisankhan20@gmail.com>
2023-03-09 19:21:14 +0530

    Password Validation

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_catalog/screens/signin_screen.dart';
+import 'package:flutter_pw_validator/Utilities/Validator.dart';
 
 import '../reusable_widgets/reusable_widget.dart';
 import '../utils/color_utils.dart';
@@ -19,6 +20,21 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  bool _obscureText = true;
+  bool isValidPass = false;
+
+  final _formKey = GlobalKey<FormState>();
+  RegExp pass_valid = RegExp(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)");
+
+  bool validatePassword(String pass) {
+    String _password = pass.trim();
+    if (pass_valid.hasMatch(_password)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   Service service = Service();
 
   TextEditingController _passwordTextController = TextEditingController();
@@ -35,60 +51,99 @@ class _SignUpScreenState extends State<SignUpScreen> {
         title: const Text("Sign Up",
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
       ),
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            hexStringToColor("CB2B93"),
-            hexStringToColor("9546C4"),
-            hexStringToColor("5E61F4")
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-          child: SingleChildScrollView(
-              child: Padding(
-            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
-            child: Column(
-              children: <Widget>[
-                /* const SizedBox(
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                hexStringToColor("CB2B93"),
+                hexStringToColor("9546C4"),
+                hexStringToColor("5E61F4")
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+                child: Column(
+                  children: <Widget>[
+                    /* const SizedBox(
                   height: 20,
                 ),
                 reusableTextField("Enter Your Name", Icons.person_outline,
                     false, _userNameTextController),
                     */
-                const SizedBox(
-                  height: 20,
-                ),
-                reusableTextField("Enter Your Email", Icons.person_outline,
-                    false, _emailTextController),
-                const SizedBox(
-                  height: 20,
-                ),
-                reusableTextField("Enter Your Password", Icons.lock_outline,
-                    false, _passwordTextController),
-                signInSignUpButton(context, false, () {
-                  if (_emailTextController.text.isNotEmpty &&
-                      _passwordTextController.text.isNotEmpty) {
-                    service.createUser(_emailTextController.text,
-                        _passwordTextController.text, context);
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    reusableTextField("Enter Your Email", Icons.person_outline,
+                        false, _emailTextController),
+                    const SizedBox(
+                      height: 20,
+                    ),
 
-                    /*  FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                            email: _emailTextController.text,
-                            password: _passwordTextController.text)
-                        .then((value) => {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => SignInScreen()))
+                    /*
+                    reusableTextField( "Enter Your Password", Icons.lock_outline, false,
+                        _passwordTextController),
+                        */
+
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter password";
+                        } else {
+                          bool result = validatePassword(value);
+                          if (result) {
+                            return null;
+                          } else {
+                            return " Password should contain Capital, small letter & Number & Special";
+                          }
+                        }
+                      },
+                      obscureText: _obscureText,
+                      controller: _passwordTextController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.white70,
+                        ),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
                             });
-                            */
-                  } else {
-                    service.error(context, "Field must not be empty");
-                  }
-                })
-              ],
-            ),
-          ))),
+                          },
+                          child: Icon(_obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                        hintText: 'Enter Your Password',
+                        hintStyle:
+                            TextStyle(color: Colors.white.withOpacity(0.9)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: const BorderSide(
+                                width: 0, style: BorderStyle.none)),
+                      ),
+                    ),
+                    signInSignUpButton(context, false, () {
+                      if (_emailTextController.text.isNotEmpty &&
+                          _passwordTextController.text.isNotEmpty) {
+                        isValidPass = _formKey.currentState!.validate();
+                        if (isValidPass) {
+                          service.createUser(_emailTextController.text,
+                              _passwordTextController.text, context);
+                        }
+                      } else {
+                        service.error(context, "Field must not be empty");
+                      }
+                    })
+                  ],
+                ),
+              ))),
+        ),
+      ),
     );
   }
 }
